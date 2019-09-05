3a4f92823ae19b31a88a85698ad407de585c9e01
Mahesh <babuveru@gmail.com>
2019-09-05 06:43:00 +0530

    5 complete

---
+++ visits_count/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const redis = require('redis');
+const process =  require('process');
+
+const app = express();
+const client = redis.createClient({
+    host: 'redis-server', // container to which we want to connect. Name is specified in docker-compose.yml
+    port: 6379 //default port number for redis on its container
+});
+
+client.set('visits', 0);
+
+app.get('/',(req,res)=>{
+    process.exit(0);
+    client.get('visits',(err, visits)=>{
+        res.send('Number of visits is '+visits);
+        client.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.listen(8081,()=>{
+    console.log('Listening on port 8081');
+})
